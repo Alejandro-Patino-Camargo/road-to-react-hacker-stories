481a22af51b221b5323afc2f09fc02e96a088091
alejandro-patino-camargo <alejandropatinoc@gmail.com>
2023-07-06 19:16:52 -0700

    Embedded JS in JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import  * as React from 'react';
 
+
+function getTitle(title){
+  return title;
+}
+
 function  App() {
   return (
   <div>
-  <h1>Hello World</h1>
-  </div>
+  <h1>Hello {getTitle('World')}</h1>
+
+    <lable htmlFor="search"> Search: </lable>
+    <input id="search" type = "text" />
+
+    </div>
   );
 }
 
