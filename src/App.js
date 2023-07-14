ddef0c067ebba4a7174eb08eb0fe61032848874d
Alejandro Patino Camargo <alejandro@Alejandros-MacBook-Pro.local>
2023-07-13 22:00:47 -0700

    firtst video, jsx lists

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
-import  * as React from 'react';
+import * as React from "react";
 
+const list = [
+  {
+    title: "vim cheat sheet",
+    author: "some nerd",
+    num_comments: "5",
+    points: "3",
+    objectID: "0",
+    url: "google.com",
+  },
+];
 
-function getTitle(title){
-  return title;
+function List() {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span> by {item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
-function  App() {
+function Search() {
   return (
-  <div>
-  <h1>Hello {getTitle('World')}</h1>
+    <div>
+      <label htmlFor="search"> Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
 
-    <lable htmlFor="search"> Search: </lable>
-    <input id="search" type = "text" />
-
+function App() {
+  return (
+    <div>
+      <h1>Hacker Stories</h1>
+      <Search />
+      <hr />
+      <List />
     </div>
   );
 }
