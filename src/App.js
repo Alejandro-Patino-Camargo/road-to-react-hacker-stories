0a24bfc18e7942b5d94bf5cbc90aa1d4d4d3d75f
alejandro-patino-camargo <alejandropatinoc@gmail.com>
2023-07-18 21:55:33 -0700

    updated todo list and components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
-import  * as React from 'react';
+import * as React from "react";
 
+const list = [
+  {
+    title: "NVIM config",
+    author: "Alejandro",
+    url: "LazyVim.com",
+    points: "5",
+    objectID: "0",
+  },
+];
 
-function getTitle(title){
-  return title;
+function List() {
+  return (
+    <ul>
+      {list.map(function (items) {
+        return (
+          <li key={items.objectID}>
+            <span>
+              <a href={items.url}>{items.title}</a>
+            </span>
+            <span> by {items.author}</span>
+            <span> {items.points}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
-function  App() {
+function Search() {
   return (
-  <div>
-  <h1>Hello {getTitle('World')}</h1>
+    <div>
+      <label htmlFor="search">Search</label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
 
-    <lable htmlFor="search"> Search: </lable>
-    <input id="search" type = "text" />
-
+function App() {
+  return (
+    <div>
+      <h1>Hacker Stories</h1>
+      <Search />
+      <hr />
+      <List />
     </div>
   );
 }
